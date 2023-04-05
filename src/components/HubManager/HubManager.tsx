4c7e69bb7cda8f7005d62e7cec10434757594f18
Taha <mt.zngn@gmail.com>
2023-04-05 22:13:50 +0100

    Added capability to add new hubs to the list

--- src/components/HubManager/HubManager.tsx
+++ src/components/HubManager/HubManager.tsx
@@ -1,5 +1,12 @@
 import React, {useState} from 'react';
-import {View, Text, FlatList, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 
 type HubStatus = 'NEW' | 'ACTIVE' | 'SUSPENDED';
 
@@ -10,19 +17,39 @@ interface Hub {
 }
 
 const HubManager: React.FC = () => {
-  const [hubs, setHubs] = useState<Hub[]>([
-    {serialNo: '123', status: 'NEW', statusDate: '01.01.20'},
-    {serialNo: '1234', status: 'NEW', statusDate: '01.01.20'},
-    {serialNo: '1235', status: 'NEW', statusDate: '01.01.20'},
-    {serialNo: 'asdf', status: 'NEW', statusDate: '01.01.20'},
-    {serialNo: 'sdffsad', status: 'NEW', statusDate: '01.01.20'},
-    {serialNo: 'sdf', status: 'NEW', statusDate: '01.01.20'},
-    {serialNo: 'sadfasd', status: 'NEW', statusDate: '01.01.20'},
-  ]);
+  const [hubs, setHubs] = useState<Hub[]>([]);
+  const [serialNo, setSerialNo] = useState<string>('');
+  const [status, setStatus] = useState<HubStatus>('NEW');
+
+  const addHub = () => {
+    const newHub: Hub = {
+      serialNo: serialNo,
+      status: status,
+      statusDate: new Date().toISOString().slice(0, 10),
+    };
+    setHubs([...hubs, newHub]);
+    setSerialNo('');
+    setStatus('NEW');
+  };
 
   return (
-    <View>
-      <Text>Hub Manager</Text>
+    <View style={styles.hubManagerContainer}>
+      <Text style={styles.title}>Hub Manager</Text>
+      <Text style={styles.inputLabel}>Serial No:</Text>
+      <TextInput
+        style={styles.hubInput}
+        value={serialNo}
+        onChangeText={text => setSerialNo(text)}
+      />
+      <Text style={styles.inputLabel}>Status:</Text>
+      <TextInput
+        style={styles.hubInput}
+        value={status}
+        onChangeText={text => setStatus(text as HubStatus)}
+      />
+      <TouchableOpacity style={styles.addHubButton} onPress={addHub}>
+        <Text>Add Hub</Text>
+      </TouchableOpacity>
       <FlatList
         data={hubs}
         keyExtractor={item => item.serialNo}
@@ -39,17 +66,46 @@ const HubManager: React.FC = () => {
 };
 
 const styles = StyleSheet.create({
+  hubManagerContainer: {
+    padding: 20,
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+  },
   hubItem: {
     borderWidth: 1,
     borderRadius: 4,
     borderColor: 'lightblue',
     padding: 10,
-    marginBottom: 10,
-    marginHorizontal: 10,
+    marginVertical: 5,
   },
   hubSerialNo: {
     fontWeight: 'bold',
     marginBottom: 5,
+  },
+  hubInput: {
+    borderWidth: 1,
+    borderColor: 'lightblue',
+    borderRadius: 4,
+    padding: 10,
+    marginBottom: 10,
+  },
+  inputLabel: {
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  addHubButton: {
+    padding: 10,
+    marginBottom: 5,
+    backgroundColor: 'lightblue',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  title: {
+    alignSelf: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 10,
   },
 });
 
